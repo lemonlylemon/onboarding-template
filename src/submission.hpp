8b53e5cb8a2a01adb6fe2dc34a3859280b0a4d6f
Tejas Rao <tejasrao980@gmail.com>
2026-09-25 23:37:36 -0400

    Add SIMD to y loop

--- src/submission.hpp
+++ src/submission.hpp
@@ -44,12 +44,6 @@
     const std::size_t numberOfRows = old_grid.rows();
     const std::size_t numberOfCols = old_grid.cols();
 
-    // for (std::size_t x = 0; x < numberOfRows; ++x) {
-    //   for (std::size_t y = 0; y < numberOfCols; ++y) {
-    //     new_grid(x,y) = old_grid(x,y); ///map old grid to the new one
-    //   }
-    // }
-
     for (std::size_t y = 0; y < numberOfCols; ++y) {
       new_grid(0, y) = old_grid(0, y);
       new_grid(numberOfRows - 1, y) = old_grid(numberOfRows - 1, y);
@@ -60,22 +54,15 @@
       new_grid(x, numberOfCols - 1) = old_grid(x, numberOfCols - 1);
     }
 
-
-
-    // for (std::size_t y = 0; y < numberOfCols; ++y) {
-    //   new_grid(0,y) = old_grid(0,y);
-    // }
-    // for (std::size_t x = 0; x < numberOfRows; ++x) {
-    //   new_grid(x,0) = old_grid(x,0);
-    // }
-    // for (std::size_t x = 1; x < numberOfRows -1 ; ++x) {
-    //   new_grid(x,numberOfCols -1 ) = old_grid(x,numberOfCols-1);
-    // }
-    // for (std::size_t y = 1; y < numberOfCols -1; ++y) {
-    //   new_grid(numberOfRows - 1,y) = old_grid(numberOfRows - 1,y);
-    // }
-
+    //Interior five point stencil
     for (std::size_t x = 1; x < numberOfRows - 1; ++x) {
+
+      // const std::size_t top = (x-1) * numberOfCols;
+      // const std::size_t current = x * numberOfCols;
+      // const std::size_t bottom = (x+1) * numberOfCols;
+
+
+#pragma omp simd
       for (std::size_t y = 1; y < numberOfCols - 1; ++y) {
         new_grid(x,y) = 0.5 * old_grid(x,y) +
                 0.125 * (old_grid(x-1,y) + old_grid(x+1,y) +
